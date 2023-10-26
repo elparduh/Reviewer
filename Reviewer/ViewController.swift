621549fca2475af813fa7268487625151574abfe
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2023-10-26 17:55:55 -0600

    first commit

--- Reviewer/ViewController.swift
+++ Reviewer/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Reviewer
-//
-//  Created by Juan Ticante Vicente on 26/10/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
   }
-
-
 }
 
